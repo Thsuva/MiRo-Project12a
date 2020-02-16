914fcf2d08e6a2ce8ad07915ce747c9602c96b8f
jacopo <jacopo.favaro94@gmail.com>
2020-02-16 11:00:43 +0100

    bug fix happy

--- src/states/happy.py
+++ src/states/happy.py
@@ -3,17 +3,17 @@ import rospy
 
 from std_msgs.msg import String,Bool,Int32
 
-# define state Idle
-class Idle(smach.State):
+# define state Happy
+class Happy(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['ok'])
-        ## Subscriber to the topic /speech_to_text a message of type String that cointains the vocal command converted in text
+
         self.pub_rgb_light = rospy.Publisher('/rgb_msg', String, queue_size=1)
 
     def execute(self, userdata):
         rospy.loginfo('Executing state HAPPY')
 	
-        self.pub_rgb_light.publish("GREEN LIGHT ON")
+        self.pub_rgb_light.publish("GREEN_LIGHT_ON")
 
         return "ok"
 
