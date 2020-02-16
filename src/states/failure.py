e8129c66fbd038135af3ea6dc00951629c240c6b
jacopo <jacopo.favaro94@gmail.com>
2020-02-16 10:56:27 +0100

    fix tab indentation

--- src/states/failure.py
+++ src/states/failure.py
@@ -3,15 +3,13 @@ import rospy
 import time
 
 from std_msgs.msg import String,Bool,Int32
-from speech_interaction.msg import Speech2text
 
 # define state Active
 class Failure(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['reset'])
 
-		self.pub_rgb_light = rospy.Publisher('rgb_msg', String, queue_size=1)
-        rospy.init_node('talker', anonymous=True)
+	self.pub_rgb_light = rospy.Publisher('/rgb_msg', String, queue_size=1)
         
     def execute(self, userdata):
         self.pub_rgb_light.publish('RED_LIGHT_ON')
