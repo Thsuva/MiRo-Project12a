0c52ea0a91d73633143d5688c4b5c4fc3c0c87e2
FZ <f.zavanone@gmail.com>
2020-02-17 17:57:56 +0100

    happy modified for test without bot added itcanbedonebetter in comments

--- src/states/happy.py
+++ src/states/happy.py
@@ -41,28 +41,35 @@ class Happy(smach.State):
         ## Initialize the state
         smach.State.__init__(self, outcomes=['ok'])
 
-	## Node rate
+	    ## Node rate
         self.rate = rospy.get_param('rate',200)
 
         ## Initialization of head capacitive sensors
+        # self.sensors_head = [0, 0, 0, 0]	
         self.h1 = 0
         self.h2 = 0
         self.h3 = 0
         self.h4 = 0
         ## Initialization of bodycapacitive sensors
+        # self.sensors_body = [0, 0, 0, 0]
         self.b1 = 0
         self.b2 = 0
         self.b3 = 0
         self.b4 = 0
 
         ## Subscriber to the topic /miro/rob01/platform/sensors a message of type platform_sensors that cointains the information about the capacitive sensors.
-        self.sub_sensors_touch = rospy.Subscriber('/miro/rob01/platform/sensors', platform_sensors, self.callback_touch,queue_size =1)
+        #self.sub_sensors_touch = rospy.Subscriber('/miro/rob01/platform/sensors', platform_sensors, self.callback_touch,queue_size =1)
         ## Publisher to the topic /miro_good a message of type platform_control which corresponds to the "Good" action.
         self.pub_platform_control = rospy.Publisher('/miro/rob01/platform/control',platform_control,queue_size=0)
-
+   
     ## Callback function that saves in class' attributes the capacitive sensor readings converted    
-    def callback_touch(self, datasensor):
-
+    #def callback_touch(self, datasensor):
+        '''
+        for i in range (0,4):
+            self.sensors_head[i] = int(fmt(datasensor.touch_head, '{0:.0f}')[i*3])
+            self.sensors_body[i] = int(fmt(datasensor.touch_body, '{0:.0f}')[i*3])
+        '''
+        '''
         self.h1 = int(fmt(datasensor.touch_head, '{0:.0f}')[0]) 
         self.h2 = int(fmt(datasensor.touch_head, '{0:.0f}')[3]) 
         self.h3 = int(fmt(datasensor.touch_head, '{0:.0f}')[6])
@@ -71,6 +78,7 @@ class Happy(smach.State):
         self.b2 = int(fmt(datasensor.touch_body, '{0:.0f}')[3]) 
         self.b3 = int(fmt(datasensor.touch_body, '{0:.0f}')[6]) 
         self.b4 = int(fmt(datasensor.touch_body, '{0:.0f}')[9])
+        '''
 
 
     ## Execute the state: MiRo has to show his happiness
@@ -85,20 +93,22 @@ class Happy(smach.State):
             
             q.sound_index_P1 = 1
 
-            if self.h1 == 1 or self.h2 == 1 or self.h3 == 1 or self.h4 == 1:
+            if self.h1 == 1 or self.h2 == 1 or self.h3 == 1 or self.h4 == 1:  # any(self.sensors_head)
                 q.eyelid_closure = 0.4
                 q.lights_raw = [255,64,64,255,64,64,255,64,64,255,64,64,255,64,64,255,64,64]
                 q.tail = 0.0
                 q.body_config = [0.2,0.5,0.2,-0.5]
                 q.body_config_speed = [0.0,-1.0,-1.0,-1.0]
+                rospy.loginfo('HEAD SENSORS TOUCH')
 
-            elif self.b1 == 1 or self.b2 == 1 or self.b3 == 1 or self.b4 == 1:
+            elif self.b1 == 1 or self.b2 == 1 or self.b3 == 1 or self.b4 == 1:  # any(self.sensors_body)
                 q.eyelid_closure = 0.1
                 q.lights_raw = [255,129,0,255,129,0,255,129,0,255,129,0,255,129,0,255,129,0]
                 q.tail = 0.0
                 q.body_config = [0.0,0.29,-0.6,-0.26]
                 q.body_config_speed = [0.0,-1.0,-1.0,-1.0]
                 q.ear_rotate = [0.5,0.5]
+                rospy.loginfo('BODY SENSORS TOUCH')
             else:
                 q.eyelid_closure = 0.0
                 q.body_config = [0.0,0.25,0.0,-0.25]
@@ -106,6 +116,7 @@ class Happy(smach.State):
                 q.tail = 68
                 q.lights_raw = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
                 q.ear_rotate = [0.0,0.0]
+                rospy.loginfo('NO SENSORS TOUCH')
             
             self.pub_platform_control.publish(q)
 
