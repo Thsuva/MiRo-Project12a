2c9b1d7dd6ee45db936acdbca083df45b79d6b56
jacopo <jacopo.favaro94@gmail.com>
2020-02-17 11:08:07 +0100

    added comments to sm_main, dummy_lfo, dummy_mto, happy and added docs folder with umls and workplan

--- src/states/dummy_mto.py
+++ src/states/dummy_mto.py
@@ -4,14 +4,22 @@ import time
 
 from std_msgs.msg import String,Bool,Int32
 
-# define state Active
+## \file dummy_mto.py 
+## \brief Class DummyMTO handles the move towards object state.
+## @n MOVE TOWARDS OBJECT has:
+## @n - output (arrived): MiRo has arrived to the goal object
 class DummyMTO(smach.State):
+
+    ## Constructor
     def __init__(self):
+        ## Initialize the state
         smach.State.__init__(self, outcomes=['arrived'])
 
+    ## Execute the state: MiRo has to get to a specific object with given shape and colour
     def execute(self, userdata):
         rospy.loginfo('Executing state DUMMY_MTO')
         
+        # This part has to be filled with the actual code (dummy file)
 	'''
 	while not rospy.is_shutdown():
             #ACTIVATION COMMAND
--- src/states/happy.py
+++ src/states/happy.py
@@ -3,17 +3,28 @@ import rospy
 
 from std_msgs.msg import String,Bool,Int32
 
-# define state Happy
+## \file happy.py 
+## \brief Class Happy handles the happy state.
+## @n HAPPY has:
+## @n - output (ok): MiRo signals that the goal has been reached
 class Happy(smach.State):
+    
+    ## Constructor
     def __init__(self):
+        ## Initialize the state
         smach.State.__init__(self, outcomes=['ok'])
 
+        ## Publisher to the topic /rgb_msg, a message of type String which signals to turn lights on and their colour
         self.pub_rgb_light = rospy.Publisher('/rgb_msg', String, queue_size=1)
 
+    ## Execute the state: MiRo has to show his happiness
     def execute(self, userdata):
         rospy.loginfo('Executing state HAPPY')
 	
+        ## Turn green lights on
         self.pub_rgb_light.publish("GREEN_LIGHT_ON")
+
+        # TODO: This part could be modified in order to show a more complex behaviour
 
         return "ok"
 
